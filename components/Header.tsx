ea3dcc1bec4de0180afbf546183f3007658ad392
c3s <165490218+code3studio@users.noreply.github.com>
2025-02-19 17:48:18 -0500

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Logo from "@/components/ui/logo";
 import Mascot from "@/components/ui/mascot";
 import ThemeToggleButton from "@/components/ui/theme-toggle-button";
 import Link from "next/link";
-import { FaGithub, FaTelegram, FaXTwitter } from "react-icons/fa6";
+import { FaGithub, FaXTwitter } from "react-icons/fa6";
 
 const Header = () => {
   return (
@@ -24,17 +24,9 @@ const Header = () => {
         <Link
           target="_blank"
           rel="noreferrer"
-          href="https://github.com/BioHazard786/Billicious"
+          href="https://x.com/pumpvolt"
         >
-          <div className="inline-flex h-9 w-9 items-center justify-center whitespace-nowrap rounded-md px-0 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50">
-            <FaTelegram className="size-5" />
-          </div>
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href="https://github.com/BioHazard786/Billicious"
-        >
+    
           <div className="inline-flex h-9 w-9 items-center justify-center whitespace-nowrap rounded-md px-0 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50">
             <FaGithub className="size-5" />
           </div>
